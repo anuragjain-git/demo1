52e1953ab8e198efec363bffe5768f6b0f7a76ba
Anurag Jain <98457054+anuragjain-git@users.noreply.github.com>
2023-12-25 03:51:49 +0530

    Create update_readme.js

---
+++ .github/workflows/update_readme.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+async function updateReadme() {
+  try {
+    // Fetch a random quote from the Quotable API
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    const randomQuote = `**Random Quote:** ${data.content} - ${data.author}`;
+
+    // Read the existing README.md file
+    let readmeContent = fs.readFileSync('README.md', 'utf-8');
+
+    // Replace the RANDOM_QUOTE_PLACEHOLDER with the new random quote
+    readmeContent = readmeContent.replace('<!-- RANDOM_QUOTE_PLACEHOLDER -->', randomQuote);
+
+    // Write the updated content back to README.md
+    fs.writeFileSync('README.md', readmeContent);
+
+    console.log('README.md updated successfully with a random quote.');
+  } catch (error) {
+    console.error('Error updating README.md:', error);
+  }
+}
+
+updateReadme();
